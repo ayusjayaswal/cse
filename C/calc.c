0295660a0a6654e028790615904bb3908c8d4b8e
ayusjayaswal <ayushjayaswal@yandex.com>
2023-08-27 12:09:15 +0530

    Update n Some more Books

--- C/calc.c
+++ C/calc.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main(){
-  int age = 18; int *pp = &age;
-  printf("The address of age: %p\n", &age);
-  printf("The address of age: %p\n", pp);
-  printf("The value of age: %d\n", age);
-  printf("The value of age: %d\n", *pp);
-  printf("age: %d\n", *pp);
-}
+int main() {
+  int c;
+
+  c = getchar();
+  while (c != EOF) {
+    putchar(c);
+    c = getchar();
+  }}
